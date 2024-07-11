3207b57e73375bb3cbbc96389d6a60776641989e
Eugene Dementiev <dementiev.eugene@gmail.com>
2024-07-11 16:01:38 +0500

    v1.0.3 with minor updates to documentation

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="pyagena",
-    version="1.0.2",
+    version="1.0.3",
     author="Erhan Pisirir, Eugene Dementiev",
     author_email="support@agenarisk.com",
-    description="Python helper library for agena.ai to create Bayesian network models from scratch or import existing models and export to agena.ai cloud or local API for calculations.",
+    description="Python utility library for agena.ai to create Bayesian network models from scratch or import existing models and export to agena.ai cloud or local API for calculations.",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/AgenaRisk/api-py",
-    download_url="https://github.com/AgenaRisk/api-py/archive/refs/tags/1.0.2.tar.gz",
+    download_url="https://github.com/AgenaRisk/api-py/archive/refs/tags/1.0.3.tar.gz",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
